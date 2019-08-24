95b21cd0cbf5deebb069fced5d4137c0f07035ca
Drizzs <drizzs@hotmail.com>
2019-08-24 01:04:17 -0400

    Biome Features
    Added features from the blocks i currently have implemented

--- src/main/java/com/minerarcana/occult/biomes/features/SpookyFeatures.java
+++ src/main/java/com/minerarcana/occult/biomes/features/SpookyFeatures.java
@@ -1,7 +1,7 @@
 package com.minerarcana.occult.biomes.features;
 
 
-import com.minerarcana.occult.biomes.features.bushes.DeepGrassFeature;
+import com.minerarcana.occult.biomes.features.bushes.*;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.IFeatureConfig;
@@ -16,6 +16,10 @@ public class SpookyFeatures
 
 
     public static final Feature<NoFeatureConfig> deepgrassfeature = registerFeature("deepgrassfeature", new DeepGrassFeature(NoFeatureConfig::deserialize));
+    public static final Feature<NoFeatureConfig> echobushfeature = registerFeature("echobushfeature", new EchoBushFeature(NoFeatureConfig::deserialize));
+    public static final Feature<NoFeatureConfig> phantombushfeature = registerFeature("phantombushfeature", new PhantomBushFeature(NoFeatureConfig::deserialize));
+    public static final Feature<NoFeatureConfig> ivyfeature = registerFeature("ivyfeature", new IvyFeature(NoFeatureConfig::deserialize));
+    public static final Feature<NoFeatureConfig> eldritchstonefeature = registerFeature("eldritchstonefeature", new EldritchStoneFeature(NoFeatureConfig::deserialize));
 
 
     private static<C extends IFeatureConfig, F extends Feature<C>> F registerFeature(String name, F feature)
---
+++ src/main/java/com/minerarcana/occult/biomes/features/bushes/PhantomBushFeature.java
@@ -0,0 +1,53 @@
+package com.minerarcana.occult.biomes.features.bushes;
+
+import com.minerarcana.occult.blocks.OccultBlocks;
+import com.mojang.datafixers.Dynamic;
+import net.minecraft.block.BlockState;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.gen.GenerationSettings;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.NoFeatureConfig;
+
+import java.util.Random;
+import java.util.function.Function;
+
+public class PhantomBushFeature extends Feature<NoFeatureConfig>
+{
+    public PhantomBushFeature(Function<Dynamic<?>, ? extends NoFeatureConfig> deserializer)
+    {
+        super(deserializer);
+    }
+
+    public BlockState chooseGrassState(Random rand)
+    {
+        return rand.nextInt(3) == 0 ? OccultBlocks.phantombush.getDefaultState() : OccultBlocks.phantombush.getDefaultState();
+    }
+
+    @Override
+    public boolean place(IWorld world, ChunkGenerator<? extends GenerationSettings> generator, Random rand, BlockPos pos, NoFeatureConfig config)
+    {
+        BlockState BlockState = this.chooseGrassState(rand);
+
+        for (BlockState BlockState1 = world.getBlockState(pos); (BlockState1.isAir(world, pos) || BlockState1.isIn(BlockTags.LEAVES)) && pos.getY() > 0; BlockState1 = world.getBlockState(pos))
+        {
+            pos = pos.down();
+        }
+
+        int i = 0;
+
+        for (int j = 0; j < 128; ++j)
+        {
+            BlockPos blockpos = pos.add(rand.nextInt(8) - rand.nextInt(8), rand.nextInt(4) - rand.nextInt(4), rand.nextInt(8) - rand.nextInt(8));
+            if (world.isAirBlock(blockpos) && BlockState.isValidPosition(world, blockpos))
+            {
+                world.setBlockState(blockpos, BlockState, 2);
+                ++i;
+            }
+        }
+
+        return i > 0;
+    }
+}
