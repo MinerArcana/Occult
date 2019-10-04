264ee4e01b9e720089591557d53078ef31abe0de
Drizzs <drizzs@hotmail.com>
2019-10-04 19:46:38 -0400

    RitualBase Work

---
+++ src/main/java/com/minerarcana/occult/common/tileentity/ritualfire/IBaseIsMultiBlock.java
@@ -0,0 +1,21 @@
+package com.minerarcana.occult.common.tileentity.ritualfire;
+
+public interface IBaseIsMultiBlock
+{
+
+    boolean isMaster();
+
+    void setMaster();
+
+    boolean isSlave();
+
+    void setSlave();
+
+    boolean isMultiBlock();
+
+    void setMultiBlock();
+
+
+
+
+}
--- src/main/java/com/minerarcana/occult/common/tileentity/ritualfire/RitualBaseTile.java
+++ src/main/java/com/minerarcana/occult/common/tileentity/ritualfire/RitualBaseTile.java
@@ -1,18 +1,26 @@
 package com.minerarcana.occult.common.tileentity.ritualfire;
 
 import com.google.common.collect.Maps;
+import com.minerarcana.occult.Occult;
+import com.minerarcana.occult.common.blocks.OccultBlocks;
+import com.minerarcana.occult.common.util.OccultTags;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.ItemStackHelper;
 import net.minecraft.inventory.container.Container;
 import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.network.play.server.SUpdateTileEntityPacket;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.Tag;
+import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
@@ -32,145 +40,228 @@ import java.util.Map;
 import static com.minerarcana.occult.common.tileentity.OccultTileEntities.RITUALBASETILE;
 import static net.minecraft.tags.ItemTags.LOGS;
 
-public class RitualBaseTile extends TileEntity implements INamedContainerProvider {
+public class RitualBaseTile extends TileEntity implements INamedContainerProvider, IBaseIsMultiBlock, ITickableTileEntity {
 
     private LazyOptional<IItemHandler> handler = LazyOptional.of(this::createHandler);
-
+    protected NonNullList<ItemStack> inventory = NonNullList.withSize(16, ItemStack.EMPTY);
     private RitualBaseTile master;
-
-    private int counter;
 
     private int burnTime;
 
-    private boolean isOn;
-    private boolean isMultiBlock;
-    private boolean isActive;
-    private boolean isCenter;
-    private boolean isMaster;
-    private boolean isSlave;
+    private boolean isMultiBlock = false;
+    private boolean isMaster = false;
+    private boolean isSlave = false;
+    private boolean isBig = false;
+    private boolean isSmall = false;
 
     public RitualBaseTile() {
         super(RITUALBASETILE);
-    }
-
-
-    public boolean isActive() {
-        if (isMultiBlock = true) {
-            if (isOn = true) {
-                isActive = true;
-            }
-        } else {
-            isActive = false;
-        }
-        return isActive;
-    }
-
-    public boolean isMultiblock() {
-        return this.isMultiBlock;
     }
 
     public boolean isOn() {
         return this.burnTime > 0;
     }
 
-    public boolean isCenter(BlockState state, World world, BlockPos pos) {
-        BlockPos east = pos.east();
-        BlockPos west = pos.west();
-        BlockPos north = pos.north();
-        BlockPos south = pos.south();
-        BlockState state1 = world.getBlockState(east);
-        BlockState state2 = world.getBlockState(west);
-        BlockState state3 = world.getBlockState(north);
-        BlockState state4 = world.getBlockState(south);
+    @Override
+    public boolean isMaster() {
+        return isMaster;
+    }
 
+    @Override
+    public void setMaster() {
 
-        if(state1.getBlock() instanceof RitualBase)
-        {
-            if(state2.getBlock() instanceof RitualBase)
-            {
-                if(state3.getBlock() instanceof RitualBase)
-                {
-                    if(state4.getBlock() instanceof RitualBase)
-                    {
+        World world = this.getWorld();
+        int small = 0;
+        int big = 0;
 
+        for (int i = 0; i < 2; ++i) {
+            for (int k = 0; k < 2; ++k) {
+                BlockPos multiblock = pos.add(i, 0, k);
+                BlockState state = world.getBlockState(multiblock).getBlockState();
+                Block block = state.getBlock();
+                if (block == OccultBlocks.ritualbase) {
+                    if (state instanceof IBaseIsMultiBlock) {
+                        if (!((IBaseIsMultiBlock) state).isMultiBlock()) {
+                            ++small;
+                        }
                     }
                 }
             }
         }
-        else{isCenter = false;}
-            return this.isCenter;
-    }
 
-    public boolean isMaster() {
-        if(this.isMultiBlock){
-        if (this.isCenter) {
-            isMaster = true;
+        if (small == 8) {
+            this.isMultiBlock = true;
+            this.isMaster = true;
+            this.isSmall = true;
+
+        }
+
+
+        if (!isSmall) {
+            for (int i = 0; i <= 2; ++i) {
+                for (int k = 0; k <= 2; ++k) {
+                    BlockPos multiblock = pos.add(i, 0, k);
+                    BlockState state = world.getBlockState(multiblock).getBlockState();
+                    Block block = state.getBlock();
+                    if (block == OccultBlocks.ritualbase) {
+                        if (state instanceof IBaseIsMultiBlock) {
+                            if (!((IBaseIsMultiBlock) state).isMultiBlock()) {
+                                ++big;
+                            }
+                        }
+                    }
+                }
+            }
+            if (big == 24) {
+                this.isMultiBlock = true;
+                this.isMaster = true;
+                this.isBig = true;
             }
         }
-        else {
-            isMaster = false;
-        }
-        return isMaster;
+
     }
 
-    public boolean isSlave() {
-        if (!this.isMaster) {
+    @Override
+    public void setSlave() {
+        if (this.isMaster) {
+            isSlave = false;
+        } else {
             isSlave = true;
         }
-        else{
-            isSlave = false;
-        }
+    }
+
+    @Override
+    public boolean isSlave() {
         return isSlave;
     }
 
+    @Override
+    public boolean isMultiBlock() {
+        return isMultiBlock;
+    }
+
+
+    @Override
+    public void setMultiBlock() {
+        int small = 0;
+        int big = 0;
+        if (!this.isMultiBlock) {
+            setMaster();
+            Occult.LOGGER.info("This is not a multiblock");
+        } else if (this.isMultiBlock && this.isMaster) {
+            if (this.isSmall) {
+                for (int i = 0; i <  2; ++i) {
+                    for (int k = 0; k < 2; ++k) {
+                        BlockPos multiblock = pos.add(i, 0, k);
+                        BlockState state = world.getBlockState(multiblock).getBlockState();
+                        Block block = state.getBlock();
+                        if (block == OccultBlocks.ritualbase) {
+                            if (state instanceof IBaseIsMultiBlock) {
+                                if (!((IBaseIsMultiBlock) state).isSlave()) {
+                                    ++small;
+                                    ((IBaseIsMultiBlock) state).setSlave();
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (small == 8) {
+                    this.isMultiBlock = true;
+                    this.isMaster = true;
+                    this.isSmall = true;
+                    Occult.LOGGER.info("This is a MultiBlock");
+
+
+                } else if (this.isBig) {
+                    for (int i = 0; i < 3; ++i) {
+                        for (int k = 0; k < 3; ++k) {
+                            BlockPos multiblock = pos.add(i, 0, k);
+                            BlockState state = world.getBlockState(multiblock).getBlockState();
+                            Block block = state.getBlock();
+                            if (block == OccultBlocks.ritualbase) {
+                                if (state instanceof IBaseIsMultiBlock) {
+                                    if (!((IBaseIsMultiBlock) state).isSlave()) {
+                                        ++big;
+                                        ((IBaseIsMultiBlock) state).setSlave();
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    if (big == 24) {
+                        this.isMultiBlock = true;
+                        this.isMaster = true;
+                        this.isBig = true;
+                        Occult.LOGGER.info("This is a MultiBlock");
+
+                    }
+                }
+            }
+
+        } else if (this.isSlave) {
+            this.isMultiBlock = true;
+        }
+
+    }
+
     public RitualBaseTile getMaster() {
-        if (this.isMaster = true) {
+        if (this.isMaster) {
             return this.master;
         }
-        return this.master;
+        return null;
     }
 
     @Override
-    public void read(CompoundNBT nbt) {
-        CompoundNBT invTag = nbt.getCompound("inv");
-        handler.ifPresent(h -> ((INBTSerializable<CompoundNBT>) h).deserializeNBT(invTag));
-
-        super.read(nbt);
-
-        if (nbt.hasUniqueId("isMaster")) {
-            this.isMaster = nbt.getBoolean("isMaster");
-            this.burnTime = nbt.getInt("BurnTime");
-        }
+    public void read(CompoundNBT tag) {
+        this.inventory = NonNullList.withSize(this.getSizeInventory(), ItemStack.EMPTY);
+        ItemStackHelper.loadAllItems(tag, this.inventory);
+        this.isMaster = tag.getBoolean("isMaster");
+        this.isSlave = tag.getBoolean("isSlave");
+        this.isMultiBlock = tag.getBoolean("isMultiBlock");
+        this.isBig = tag.getBoolean("isBig");
+        this.isSmall = tag.getBoolean("isSmall");
+        super.read(tag);
+        this.burnTime = tag.getInt("BurnTime");
     }
 
     @Override
-    public CompoundNBT write(CompoundNBT nbt) {
+    public CompoundNBT write(CompoundNBT tag) {
         handler.ifPresent(h -> {
             CompoundNBT compound = ((INBTSerializable<CompoundNBT>) h).serializeNBT();
-            nbt.put("inv", compound);
+            tag.put("inv", compound);
         });
-        nbt.putBoolean("isMaster", this.isMaster);
-        nbt.putBoolean("isSlave", this.isSlave);
-        nbt.putInt("counter", counter);
-        nbt.putInt("BurnTime", this.burnTime);
+        tag.putBoolean("isBig", this.isBig);
+        tag.putBoolean("isSmall", this.isSmall);
+        tag.putBoolean("isMultiBlock", this.isMultiBlock);
+        tag.putBoolean("isMaster", this.isMaster);
+        tag.putBoolean("isSlave", this.isSlave);
+        tag.putInt("BurnTime", this.burnTime);
 
-        return super.write(nbt);
+        return super.write(tag);
     }
 
     @Nonnull
     @Override
     public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
-            if (isMaster) {
-                if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
-                    return handler.cast();
-                }
+        if (isMaster) {
+            if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+                return handler.cast();
             }
+        }
 
         return super.getCapability(cap, side);
     }
 
     private IItemHandler createHandler() {
-        return new ItemStackHandler(1) {
+        return new ItemStackHandler(16) {
+
+            @Override
+            protected int getStackLimit(int slot, @Nonnull ItemStack stack) {
+                return 1;
+            }
+
 
             @Override
             protected void onContentsChanged(int slot) {
@@ -178,14 +269,8 @@ public class RitualBaseTile extends TileEntity implements INamedContainerProvide
             }
 
             @Override
-            public int getSlotLimit(int slot)
-            {
-                return 9;
-            }
-
-            @Override
             public boolean isItemValid(int slot, @Nonnull ItemStack stack) {
-                return stack.getItem().getTags() == LOGS;
+                return stack.getItem().isIn(LOGS);
             }
 
             @Nonnull
@@ -196,6 +281,7 @@ public class RitualBaseTile extends TileEntity implements INamedContainerProvide
                 }
                 return super.insertItem(slot, stack, simulate);
             }
+
         };
 
 
@@ -231,6 +317,17 @@ public class RitualBaseTile extends TileEntity implements INamedContainerProvide
     @Override
     public Container createMenu(int i, PlayerInventory playerInventory, PlayerEntity playerEntity) {
         return new RitualBaseContainer(i, world, pos, playerInventory);
+    }
+
+    public int getSizeInventory() {
+        return this.inventory.size();
+    }
+
+    @Override
+    public void tick() {
+        if(!this.isMaster) {
+            setMultiBlock();
+        }
     }
 }
 
