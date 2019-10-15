782254c2487d97a62436b453cf8fc25e967d4b50
Drizzs <drizzs@hotmail.com>
2019-10-15 04:31:42 -0400

    EldritchStone

--- src/main/java/com/minerarcana/occult/blocks/tileentity/EldritchStoneTileEntity.java
+++ src/main/java/com/minerarcana/occult/blocks/tileentity/EldritchStoneTileEntity.java
@@ -2,14 +2,19 @@ package com.minerarcana.occult.blocks.tileentity;
 
 import com.google.common.collect.Maps;
 import com.minerarcana.occult.api.pressure.PressureType;
+import com.minerarcana.occult.blocks.EldritchStone;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
 
 import java.util.Map;
 
 
 import static com.minerarcana.occult.util.lib.OccultNameLib.ELDRITCHSTONETILEENTITY;
+import static com.minerarcana.occult.util.lib.OccultNameLib.transformedstone;
 
-public class EldritchStoneTileEntity extends TileEntity {
+public class EldritchStoneTileEntity extends TileEntity implements ITickableTileEntity {
 
 
     public final Map<PressureType, Integer> pressures;
@@ -20,4 +25,15 @@ public class EldritchStoneTileEntity extends TileEntity {
     }
 
 
+    @Override
+    public void tick() {
+        BlockState teBlockAbove = world.getBlockState(pos.up());
+        BlockState teBlockAbove2 = world.getBlockState(pos.up(2));
+
+        if (teBlockAbove.getBlock() instanceof EldritchStone && teBlockAbove2.getBlock() instanceof EldritchStone) {
+            world.setBlockState(pos, transformedstone.getDefaultState());
+            world.setBlockState(pos.up(), Blocks.AIR.getDefaultState());
+            world.setBlockState(pos.up(2), Blocks.AIR.getDefaultState());
+        }
+    }
 }
--- src/main/java/com/minerarcana/occult/blocks/tileentity/TransformedStoneTileEntity.java
+++ src/main/java/com/minerarcana/occult/blocks/tileentity/TransformedStoneTileEntity.java
@@ -10,23 +10,13 @@ import net.minecraftforge.fml.common.Mod;
 import static com.minerarcana.occult.util.lib.OccultNameLib.*;
 
 
-@Mod.EventBusSubscriber
+
 public class TransformedStoneTileEntity extends TileEntity {
 
     public TransformedStoneTileEntity() {
         super(TRANSFORMEDSTONETILEENTITY);
     }
 
-    @SubscribeEvent
-    public void TransformEldritchStone() {
-        BlockState teBlockAbove = world.getBlockState(pos.up());
-        BlockState teBlockAbove2 = world.getBlockState(pos.up(2));
 
-        if (teBlockAbove.getBlock() instanceof EldritchStone && teBlockAbove2.getBlock() instanceof EldritchStone) {
-                world.setBlockState(pos, transformedstone.getDefaultState());
-                world.setBlockState(pos.up(), Blocks.AIR.getDefaultState());
-                world.setBlockState(pos.up(2), Blocks.AIR.getDefaultState());
-        }
-    }
 
 }
