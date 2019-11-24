20e68794d8bd7dd90563eae5a2120b953bb5058e
Drizzs <drizzs@hotmail.com>
2019-11-24 14:23:27 -0500

    Refractor Mistake

--- src/main/java/com/minerarcana/occult/items/liontools/OccultArmorTypes.java
+++ src/main/java/com/minerarcana/occult/items/liontools/OccultArmorTypes.java
@@ -1,65 +1,80 @@
 package com.minerarcana.occult.items.liontools;
 
 
-import net.minecraft.item.IItemTier;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.IArmorMaterial;
 import net.minecraft.item.Item;
 import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
 
+import static com.minerarcana.occult.Occult.MOD_ID;
 import static com.minerarcana.occult.content.OccultItems.HUNGRY_LIONMETAL_INGOT;
 import static com.minerarcana.occult.content.OccultItems.SATED_LIONMETAL_INGOT;
 
-public enum OccultArmorTypes implements IItemTier {
+public enum OccultToolTypes implements IArmorMaterial {
 
-    sated_lion_tools(10.0f, 9.0f, 800, 3, 25, SATED_LIONMETAL_INGOT.get()),
-    hungry_lion_tools(10.0f, 9.0f, 800, 3, 25, HUNGRY_LIONMETAL_INGOT.get());
+    satedlionarmortype("sated_lion_armor", new int[] {2, 2, 2, 2}, 50, SATED_LIONMETAL_INGOT.get(), "entity.ender_dragon.growl", 0.0f),
+    hungrylionarmortype("hungry_lion_armor", new int[] {2, 2, 2, 2}, 25, HUNGRY_LIONMETAL_INGOT.get(), "entity.ender_dragon.growl", 0.0f);
 
-    private float attackDamage, efficiency;
-    private int durability, harvestLevel, enchantability;
-    private Item repairMaterial;
+    private String name, equipSound;
+    private int enchantability;
+    private Item repairItem;
+    private int[] damageReductionAmounts;
+    private float toughness;
 
-    private OccultArmorTypes(float attackDamage, float efficiency, int durability, int harvestLevel, int enchantability, Item repairMaterial)
+    private OccultToolTypes(String name, int[] damageReductionAmounts, int enchantability, Item repairItem, String equipSound, float toughness)
     {
-        this.attackDamage = attackDamage;
-        this.efficiency = efficiency;
-        this.durability = durability;
-        this.harvestLevel = harvestLevel;
+        this.name = name;
+        this.equipSound = equipSound;
         this.enchantability = enchantability;
-        this.repairMaterial = repairMaterial;
+        this.repairItem = repairItem;
+        this.damageReductionAmounts = damageReductionAmounts;
+        this.toughness = toughness;
     }
 
     @Override
-    public float getAttackDamage()
-    {
-        return this.attackDamage;
+    public int getDurability(EquipmentSlotType slotIn) {
+        int durability = 0;
+        if (slotIn.equals(EquipmentSlotType.HEAD)) {
+            durability = 200;
+        } else if (slotIn.equals(EquipmentSlotType.CHEST)) {
+            durability = 200;
+        } else if (slotIn.equals(EquipmentSlotType.LEGS)) {
+            durability = 200;
+        } else if (slotIn.equals(EquipmentSlotType.FEET)) {
+            durability = 200;
+        }
+        return durability;
     }
 
     @Override
-    public float getEfficiency()
-    {
-        return this.efficiency;
+    public int getDamageReductionAmount(EquipmentSlotType slotIn) {
+        return this.damageReductionAmounts[slotIn.getIndex()];
     }
 
     @Override
-    public int getEnchantability()
-    {
+    public int getEnchantability() {
         return this.enchantability;
     }
 
     @Override
-    public int getHarvestLevel()
-    {
-        return this.harvestLevel;
+    public SoundEvent getSoundEvent() {
+        return new SoundEvent(new ResourceLocation(equipSound));
     }
 
     @Override
-    public int getMaxUses()
-    {
-        return this.durability;
+    public Ingredient getRepairMaterial() {
+        return Ingredient.fromItems(this.repairItem);
     }
 
     @Override
-    public Ingredient getRepairMaterial()
-    {
-        return Ingredient.fromItems(this.repairMaterial);
+    public String getName() {
+        return MOD_ID + ":" + this.name;
+    }
+
+    @Override
+    public float getToughness() {
+        return 0;
     }
 }
