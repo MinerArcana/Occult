4e2a83fcd7db7a787edaa10c52f97aa1a5be92a9
Drizzs <drizzs@hotmail.com>
2020-07-09 01:10:16 -0400

    PileBlock, Needs models + resources
    +

--- src/main/java/minerarcana/occult/blocks/base/PileBlock.java
+++ src/main/java/minerarcana/occult/blocks/base/PileBlock.java
@@ -1,27 +1,247 @@
 package minerarcana.occult.blocks.base;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import minerarcana.occult.content.OccultBlocks;
+import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.EnumProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.state.properties.RedstoneSide;
+import net.minecraft.util.Direction;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.shapes.IBooleanFunction;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
-import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.IWorldReader;
+import net.minecraft.world.World;
 
-import java.util.stream.Stream;
+import java.util.Map;
 
 public class PileBlock extends Block {
 
+    public static final EnumProperty<RedstoneSide> NORTH = BlockStateProperties.REDSTONE_NORTH;
+    public static final EnumProperty<RedstoneSide> EAST = BlockStateProperties.REDSTONE_EAST;
+    public static final EnumProperty<RedstoneSide> SOUTH = BlockStateProperties.REDSTONE_SOUTH;
+    public static final EnumProperty<RedstoneSide> WEST = BlockStateProperties.REDSTONE_WEST;
+
+    public static final Map<Direction, EnumProperty<RedstoneSide>> FACING_PROPERTY_MAP = Maps.newEnumMap(ImmutableMap.of(Direction.NORTH, NORTH, Direction.EAST, EAST, Direction.SOUTH, SOUTH, Direction.WEST, WEST));
+
+    protected static final VoxelShape[] SHAPES = new VoxelShape[]{Block.makeCuboidShape(3.0D, 0.0D, 3.0D, 13.0D, 1.0D, 13.0D), Block.makeCuboidShape(3.0D, 0.0D, 3.0D, 13.0D, 1.0D, 16.0D), Block.makeCuboidShape(0.0D, 0.0D, 3.0D, 13.0D, 1.0D, 13.0D), Block.makeCuboidShape(0.0D, 0.0D, 3.0D, 13.0D, 1.0D, 16.0D), Block.makeCuboidShape(3.0D, 0.0D, 0.0D, 13.0D, 1.0D, 13.0D), Block.makeCuboidShape(3.0D, 0.0D, 0.0D, 13.0D, 1.0D, 16.0D), Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 13.0D, 1.0D, 13.0D), Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 13.0D, 1.0D, 16.0D), Block.makeCuboidShape(3.0D, 0.0D, 3.0D, 16.0D, 1.0D, 13.0D), Block.makeCuboidShape(3.0D, 0.0D, 3.0D, 16.0D, 1.0D, 16.0D), Block.makeCuboidShape(0.0D, 0.0D, 3.0D, 16.0D, 1.0D, 13.0D), Block.makeCuboidShape(0.0D, 0.0D, 3.0D, 16.0D, 1.0D, 16.0D), Block.makeCuboidShape(3.0D, 0.0D, 0.0D, 16.0D, 1.0D, 13.0D), Block.makeCuboidShape(3.0D, 0.0D, 0.0D, 16.0D, 1.0D, 16.0D), Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 1.0D, 13.0D), Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 1.0D, 16.0D)};
+
+
     public PileBlock() {
         super(Block.Properties.create(Material.SAND).notSolid().noDrops());
+        this.setDefaultState(this.stateContainer.getBaseState().with(NORTH, RedstoneSide.NONE).with(EAST, RedstoneSide.NONE).with(SOUTH, RedstoneSide.NONE).with(WEST, RedstoneSide.NONE));
     }
 
     @Override
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return SHAPES[getAABBIndex(state)];
+    }
 
-        return Stream.of(Block.makeCuboidShape(5D, 0.0D, 5D, 11D, 1D, 11D),Block.makeCuboidShape(6D, 1D, 6D, 10D, 2D, 10D),Block.makeCuboidShape(7D, 2D, 7D, 9D, 3D, 9D)).reduce((v1, v2) -> {return VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR);}).get();
+    private static int getAABBIndex(BlockState state) {
+        int i = 0;
+        boolean flag = state.get(NORTH) != RedstoneSide.NONE;
+        boolean flag1 = state.get(EAST) != RedstoneSide.NONE;
+        boolean flag2 = state.get(SOUTH) != RedstoneSide.NONE;
+        boolean flag3 = state.get(WEST) != RedstoneSide.NONE;
+        if (flag || flag2 && !flag && !flag1 && !flag3) {
+            i |= 1 << Direction.NORTH.getHorizontalIndex();
+        }
+
+        if (flag1 || flag3 && !flag && !flag1 && !flag2) {
+            i |= 1 << Direction.EAST.getHorizontalIndex();
+        }
+
+        if (flag2 || flag && !flag1 && !flag2 && !flag3) {
+            i |= 1 << Direction.SOUTH.getHorizontalIndex();
+        }
+
+        if (flag3 || flag1 && !flag && !flag2 && !flag3) {
+            i |= 1 << Direction.WEST.getHorizontalIndex();
+        }
+
+        return i;
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        IBlockReader iblockreader = context.getWorld();
+        BlockPos blockpos = context.getPos();
+        return this.getDefaultState().with(WEST, this.getSide(iblockreader, blockpos, Direction.WEST)).with(EAST, this.getSide(iblockreader, blockpos, Direction.EAST)).with(NORTH, this.getSide(iblockreader, blockpos, Direction.NORTH)).with(SOUTH, this.getSide(iblockreader, blockpos, Direction.SOUTH));
+    }
+
+    @Override
+    public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
+        if (facing == Direction.DOWN) {
+            return stateIn;
+        } else {
+            return facing == Direction.UP ? stateIn.with(WEST, this.getSide(worldIn, currentPos, Direction.WEST)).with(EAST, this.getSide(worldIn, currentPos, Direction.EAST)).with(NORTH, this.getSide(worldIn, currentPos, Direction.NORTH)).with(SOUTH, this.getSide(worldIn, currentPos, Direction.SOUTH)) : stateIn.with(FACING_PROPERTY_MAP.get(facing), this.getSide(worldIn, currentPos, facing));
+        }
+    }
+
+    @Override
+    public void updateDiagonalNeighbors(BlockState state, IWorld worldIn, BlockPos pos, int flags) {
+        try (BlockPos.PooledMutable blockpos$pooledmutable = BlockPos.PooledMutable.retain()) {
+            for (Direction direction : Direction.Plane.HORIZONTAL) {
+                RedstoneSide redstoneside = state.get(FACING_PROPERTY_MAP.get(direction));
+                if (redstoneside != RedstoneSide.NONE && worldIn.getBlockState(blockpos$pooledmutable.setPos(pos).move(direction)).getBlock() != this) {
+                    blockpos$pooledmutable.move(Direction.DOWN);
+                    BlockState blockstate = worldIn.getBlockState(blockpos$pooledmutable);
+                    if (blockstate.getBlock() != Blocks.OBSERVER) {
+                        BlockPos blockpos = blockpos$pooledmutable.offset(direction.getOpposite());
+                        BlockState blockstate1 = blockstate.updatePostPlacement(direction.getOpposite(), worldIn.getBlockState(blockpos), worldIn, blockpos$pooledmutable, blockpos);
+                        replaceBlock(blockstate, blockstate1, worldIn, blockpos$pooledmutable, flags);
+                    }
+
+                    blockpos$pooledmutable.setPos(pos).move(direction).move(Direction.UP);
+                    BlockState blockstate3 = worldIn.getBlockState(blockpos$pooledmutable);
+                    if (blockstate3.getBlock() != Blocks.OBSERVER) {
+                        BlockPos blockpos1 = blockpos$pooledmutable.offset(direction.getOpposite());
+                        BlockState blockstate2 = blockstate3.updatePostPlacement(direction.getOpposite(), worldIn.getBlockState(blockpos1), worldIn, blockpos$pooledmutable, blockpos1);
+                        replaceBlock(blockstate3, blockstate2, worldIn, blockpos$pooledmutable, flags);
+                    }
+                }
+            }
+        }
 
     }
+
+    private RedstoneSide getSide(IBlockReader worldIn, BlockPos pos, Direction face) {
+        BlockPos blockpos = pos.offset(face);
+        BlockState blockstate = worldIn.getBlockState(blockpos);
+        BlockPos blockpos1 = pos.up();
+        BlockState blockstate1 = worldIn.getBlockState(blockpos1);
+        if (!blockstate1.isNormalCube(worldIn, blockpos1)) {
+            boolean flag = blockstate.isSolidSide(worldIn, blockpos, Direction.UP) || blockstate.getBlock() == Blocks.HOPPER;
+            if (flag && canConnectTo(worldIn.getBlockState(blockpos.up()))) {
+                if (blockstate.isCollisionShapeOpaque(worldIn, blockpos)) {
+                    return RedstoneSide.UP;
+                }
+
+                return RedstoneSide.SIDE;
+            }
+        }
+
+        return !canConnectTo(blockstate) && (blockstate.isNormalCube(worldIn, blockpos) || !canConnectTo(worldIn.getBlockState(blockpos.down()))) ? RedstoneSide.NONE : RedstoneSide.SIDE;
+    }
+
+    public boolean isValidPosition(BlockState state, IWorldReader worldIn, BlockPos pos) {
+        BlockPos blockpos = pos.down();
+        BlockState blockstate = worldIn.getBlockState(blockpos);
+        return blockstate.isSolidSide(worldIn, blockpos, Direction.UP) || blockstate.getBlock() == Blocks.HOPPER;
+    }
+
+    private void notifyWireNeighborsOfStateChange(World worldIn, BlockPos pos) {
+        if (worldIn.getBlockState(pos).getBlock() == this) {
+            worldIn.notifyNeighborsOfStateChange(pos, this);
+
+            for (Direction direction : Direction.values()) {
+                worldIn.notifyNeighborsOfStateChange(pos.offset(direction), this);
+            }
+
+        }
+    }
+
+    public void onBlockAdded(BlockState state, World worldIn, BlockPos pos, BlockState oldState, boolean isMoving) {
+        if (oldState.getBlock() != state.getBlock() && !worldIn.isRemote) {
+
+            for (Direction direction : Direction.Plane.VERTICAL) {
+                worldIn.notifyNeighborsOfStateChange(pos.offset(direction), this);
+            }
+
+            for (Direction direction1 : Direction.Plane.HORIZONTAL) {
+                this.notifyWireNeighborsOfStateChange(worldIn, pos.offset(direction1));
+            }
+
+            for (Direction direction2 : Direction.Plane.HORIZONTAL) {
+                BlockPos blockpos = pos.offset(direction2);
+                if (worldIn.getBlockState(blockpos).isNormalCube(worldIn, blockpos)) {
+                    this.notifyWireNeighborsOfStateChange(worldIn, blockpos.up());
+                } else {
+                    this.notifyWireNeighborsOfStateChange(worldIn, blockpos.down());
+                }
+            }
+
+        }
+    }
+
+    public void onReplaced(BlockState state, World worldIn, BlockPos pos, BlockState newState, boolean isMoving) {
+        if (!isMoving && state.getBlock() != newState.getBlock()) {
+            super.onReplaced(state, worldIn, pos, newState, isMoving);
+            if (!worldIn.isRemote) {
+                for (Direction direction : Direction.values()) {
+                    worldIn.notifyNeighborsOfStateChange(pos.offset(direction), this);
+                }
+
+                for (Direction direction1 : Direction.Plane.HORIZONTAL) {
+                    this.notifyWireNeighborsOfStateChange(worldIn, pos.offset(direction1));
+                }
+
+                for (Direction direction2 : Direction.Plane.HORIZONTAL) {
+                    BlockPos blockpos = pos.offset(direction2);
+                    if (worldIn.getBlockState(blockpos).isNormalCube(worldIn, blockpos)) {
+                        this.notifyWireNeighborsOfStateChange(worldIn, blockpos.up());
+                    } else {
+                        this.notifyWireNeighborsOfStateChange(worldIn, blockpos.down());
+                    }
+                }
+
+            }
+        }
+    }
+
+    public void neighborChanged(BlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos, boolean isMoving) {
+        if (!worldIn.isRemote) {
+            if (state.isValidPosition(worldIn, pos)) {
+            } else {
+                spawnDrops(state, worldIn, pos);
+                worldIn.removeBlock(pos, false);
+            }
+
+        }
+    }
+
+    protected boolean canConnectTo(BlockState blockState) {
+        Block block = blockState.getBlock();
+        if (block == OccultBlocks.SACRED_SALT.getBlock()) {
+            return true;
+        }
+        return false;
+    }
+
+    public BlockState rotate(BlockState state, Rotation rot) {
+        switch(rot) {
+            case CLOCKWISE_180:
+                return state.with(NORTH, state.get(SOUTH)).with(EAST, state.get(WEST)).with(SOUTH, state.get(NORTH)).with(WEST, state.get(EAST));
+            case COUNTERCLOCKWISE_90:
+                return state.with(NORTH, state.get(EAST)).with(EAST, state.get(SOUTH)).with(SOUTH, state.get(WEST)).with(WEST, state.get(NORTH));
+            case CLOCKWISE_90:
+                return state.with(NORTH, state.get(WEST)).with(EAST, state.get(NORTH)).with(SOUTH, state.get(EAST)).with(WEST, state.get(SOUTH));
+            default:
+                return state;
+        }
+    }
+
+    public BlockState mirror(BlockState state, Mirror mirrorIn) {
+        switch(mirrorIn) {
+            case LEFT_RIGHT:
+                return state.with(NORTH, state.get(SOUTH)).with(SOUTH, state.get(NORTH));
+            case FRONT_BACK:
+                return state.with(EAST, state.get(WEST)).with(WEST, state.get(EAST));
+            default:
+                return super.mirror(state, mirrorIn);
+        }
+    }
+
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(NORTH, EAST, SOUTH, WEST);
+    }
+
 }
